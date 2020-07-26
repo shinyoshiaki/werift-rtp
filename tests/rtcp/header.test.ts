40fc2fb1b07babc8463c11018342f253254f91e6
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-07-26 17:00:46 +0900

    refactor

---
+++ tests/rtcp/header.test.ts
@@ -0,0 +1,14 @@
+import { RtcpHeader } from "../../src/rtcp/header";
+
+describe("rtcp/header", () => {
+  test("valid", () => {
+    const raw = Buffer.from([0x81, 0xc9, 0x00, 0x07]);
+    const h = new RtcpHeader({
+      padding: false,
+      count: 1,
+      type: 201,
+      length: 7,
+    });
+    expect(h.serialize()).toEqual(raw);
+  });
+});
